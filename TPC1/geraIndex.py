271908ec98f39fe22a332b8bb627ca98c2204b30
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2024-02-06 13:17:09 +0000

    Update CSS link in geraIndex.py and generatePages.py

--- TPC1/geraIndex.py
+++ TPC1/geraIndex.py
@@ -11,7 +11,7 @@ preHTML = """
         <title>Ruas de Braga</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" >
-        <link rel="stylesheet" href="../w3.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     </head>
 
     <body>
