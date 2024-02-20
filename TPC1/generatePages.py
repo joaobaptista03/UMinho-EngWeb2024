ad7eb93018f806d03e193be719fff879a2ae306c
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2024-02-20 09:43:05 +0000

    tpc2

--- TPC1/generatePages.py
+++ TPC1/generatePages.py
@@ -11,7 +11,7 @@ preHTML = """
         <title>{title}</title>
         <meta charset="UTF-8">
         <meta nome="viewport" content="width=device-width, initial-scale=1" >
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="../w3.css">
     </head>
     <body>
         <div class="w3-card-4">
---
+++ TPC2/servidor.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    var q = url.parse(req.url, true).pathname.slice(1);
+
+    if (q === '') {
+        serveFile('cidadesSite/index.html', 'text/html; charset=utf-8', res);
+    } else if (q === 'w3.css') {
+        serveFile('w3.css', 'text/css', res);
+    } else {
+        fs.readFile('mapa-virtual.json', function(err, data) {
+            if (err) {
+                sendError(res);
+            } else {
+                var ids = JSON.parse(data).cidades.map(cidade => cidade.id);
+                if (ids.includes(q)) {
+                    serveFile('cidadesSite/' + q + '.html', 'text/html; charset=utf-8', res);
+                } else {
+                    console.log('404: ' + q);
+                }
+            }
+        });
+    }
+}).listen(7777);
+
+function serveFile(filePath, contentType, res) {
+    fs.readFile(filePath, function(err, data) {
+        if (err) {
+            sendError(res);
+        } else {
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(data);
+            res.end();
+        }
+    });
+}
+
+function sendError(res) {
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.write('Error reading file');
+    res.end();
+}
