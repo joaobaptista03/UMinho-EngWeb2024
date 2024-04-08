6fd024096d7e9b9024105e481c1fc5ae65212f26
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2024-04-08 17:46:06 +0100

    continuacao

---
+++ Ficha_Aferição/script.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+json_file_paths = [
+    "datasets/dataset-extra1.json",
+    "datasets/dataset-extra2.json",
+    "datasets/dataset-extra3.json"
+]
+
+api_endpoint = "http://localhost:7777/pessoas"
+
+for json_file_path in json_file_paths:
+    with open(json_file_path, "r") as json_file:
+        json_data = json.load(json_file)
+
+        for data in json_data:
+            print("Nome: ", data["nome"])
+            
+            response = requests.post(api_endpoint, json=data, headers={"Content-Type": "application/json"})
+            
+            if response.status_code == 200:
+                print("Data sent successfully")
+            else:
+                print("Failed to send data. Status code:", response.status_code)
